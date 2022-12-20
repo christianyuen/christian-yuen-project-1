7d838108911ec2f1b199c97f688fd301a604adb1
Christian Yuen <christianyuen1@gmail.com>
2022-12-20 09:49:03 -0500

    added JS for blogpost

---
+++ script.js
@@ -0,0 +1,60 @@
+// ADDING A BLOG POST
+
+// select our <form> element
+const formElement = document.querySelector('form');
+
+// add a 'submit' event listener to the <form>
+formElement.addEventListener('submit', function (event) {
+    console.log(event)
+    event.preventDefault(); // prevents auto refresh after form is 'SUBMIT'
+
+    
+    // get the post from the <input> element 
+    const inputElement1 = document.getElementById('name');
+    const inputElement2 = document.getElementById('email');
+    const inputElement3 = document.getElementById('message');
+
+    // find value of the <input> element and slap task in a variable (const)
+    const post1 = inputElement1.value
+    const post2 = inputElement2.value
+    const post3 = inputElement3.value
+
+    console.log(post1);
+    console.log(post2);
+    console.log(post3);
+
+    // only log the task and reset the input if there is a post
+    if (post1, post2, post3) {
+
+        // create a <p> element that contains the blog text
+        const pItemElement1 = document.createElement('p');
+        const pItemElement2 = document.createElement('p');
+        const pItemElement3 = document.createElement('p');
+
+        // add the post text to the paragraph element
+        pItemElement1.textContent = `${post1}`;
+        pItemElement2.textContent = `${post2}`;
+        pItemElement3.textContent = `${post3}`;
+
+        console(pItemElement1);
+
+        // select the div element
+        divElement = document.querySelectorAll('paragraph');
+
+        // append paragraph element to div item element
+        divElement.append(pItemElement1);
+
+        // select/target the <ul> element
+        // const ulElement = document.querySelector('ul');
+
+        // append the newly created <li> element to the <ul> element
+        // ulElement.appendChild(listItemElement);
+
+        // reset the input
+        inputElement1.value = '';
+        inputElement2.value = '';
+        inputElement3.value = '';
+    }
+}); 
+
+
